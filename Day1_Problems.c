7a155982a0a9bbfdccea50e6cd39370f455df007
FirewallForge <FirewallForge@example.com>
2025-06-17 10:01:15 +0530

    Added solutions for Day 1 problems

--- Day1_Problems.c
+++ Day1_Problems.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
 
+void checkEvenOrOdd(int num)
+ {
+    if (num % 2 == 0) 
+    {
+        printf("%d is Even\n", num);
+    } 
+    else 
+    {
+        printf("%d is Odd\n", num);
+    }
+}
+
+void printNaturalNumbers(int n)
+ {
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%d ", i);
+    }
+    printf("\n");
+}
+
+int main() 
+{
+    int number, n;
+    
+    printf("Enter a number to check even or odd: ");
+    scanf("%d", &number);
+    checkEvenOrOdd(number);
+    
+    printf("Enter a number to print n natural numbers: ");
+    scanf("%d", &n);
+    printNaturalNumbers(n);
+    
+    return 0;
+}
